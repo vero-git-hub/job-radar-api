74de59c1f13acfe53e653a5d7800fba019444894
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-09-13 22:07:20 +0300

    Initial

---
+++ src/main/java/org/example/jobradarapi/JobRadarApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobradarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRadarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRadarApiApplication.class, args);
+    }
+
+}
